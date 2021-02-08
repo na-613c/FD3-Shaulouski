68916c2f00aee1922cd79a302adce8e9f2479198
na-613c <na613c613c@gmail.com>
2021-02-09 00:24:34 +0300

    first lesson

---
+++ ishop/ShopTable.js
@@ -0,0 +1,36 @@
+var ShopTable = React.createClass({
+
+    displayName: 'ShopTable',
+
+    getDefaultProps: function () {
+        return {
+            shopName: "Проверьте название магазина",
+            productsArr: [{ id: 0, name: 'нет данных', price: 'нет данных', url: 'нет данных', counter: 'нет данных' }]
+        }
+    },
+
+    render: function () {
+
+        const products = this.props.productsArr.map((e) => {
+            return React.DOM.tr({ key: e.id, className: 'Product' },
+                React.DOM.td({ className: 'productsImg' },
+                    React.DOM.img({ src: e.url },)),
+                React.DOM.td({}, e.name),
+                React.DOM.td({}, `${e.price} руб`),
+                React.DOM.td({}, `${e.count} шт`),
+            );
+        })
+
+        const theadArr = ["URL фотографии", "Название", "Цена", "Количество"].map((e) => {
+            return React.DOM.td({ key: e, className: 'ProductsThead' }, e);
+        })
+
+        return React.DOM.table({ className: 'ShopTable' },
+            React.DOM.caption({ className: 'ShopCaption' }, this.props.shopName),
+            React.DOM.thead({ className: 'ShopName' },
+                React.DOM.tr({}, theadArr)),
+            React.DOM.tbody({}, products),
+        );
+    },
+
+});
